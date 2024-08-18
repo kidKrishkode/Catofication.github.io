0d9dc3dfbef0cd212d88b7e2e9df47ee7c4a9309
Krishnendu Mitra <150600460+kidKrishkode@users.noreply.github.com>
2024-08-18 19:19:35 +0530

    Version 1.0

---
+++ public/App.js
@@ -0,0 +1,85 @@
+let nav = 0;
+let system;
+let loader;
+let temp;
+
+function System(){
+    this.listen = window.location;
+}
+function Loader(load){
+    this.loaded = load;
+}
+document.addEventListener("DOMContentLoaded",() => {
+    loader = new Loader(true);
+    loader.creat();
+    loader.remove(2000);
+    system = new System();
+    document.getElementById('side-menu').innerHTML = '<div class="hambarger-menu"><ul class="nav justify-content-end">'+document.getElementById('nav-menu').innerHTML+'</ul></div>';
+});
+function navbar_toggle(){
+    if(nav==0){
+        document.getElementById('side-menu').style.display = "block";
+        document.body.style.overflowY = "hidden";
+        nav++;
+    }else{
+        document.getElementById('side-menu').style.display = "none";
+        document.body.style.overflowY = "auto";
+        nav--;
+    }
+}
+Loader.prototype.creat = function(){
+    if(loader.loaded!=false){
+        const loaderEle = document.createElement('div');
+        loaderEle.classList.add("loader");
+        loaderEle.innerHTML = `<div class="centerDia"><div class="loading"></div></div>`;
+        document.body.appendChild(loaderEle);
+        document.body.style.overflowY = "hidden";
+    }
+}
+Loader.prototype.remove = function(time){
+    if(time<100){
+        return false;
+    }
+    setTimeout(()=>{
+        document.body.removeChild(document.querySelector('.loader'));
+        loader.loaded = false;
+        document.body.style.overflowY = "scroll";
+    },time);
+}
+function route(link){
+    window.location = link;
+}
+function invaild(){
+    alert("Sorry, this feature not avalible in this version,\nTry another one!...");
+}
+function search_product(data,list){
+    let find=miss=0;
+    let input = document.getElementById(`${data}`).value;
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName(`${list}`);
+    for(i = 0; i<x.length; i++){ 
+        if(!x[i].innerHTML.toLowerCase().includes(input)){
+            x[i].style.display="none";
+            miss++;
+        }else{
+            x[i].style.display="list-item";
+            find++;
+        }
+    }
+    if(data=='searchSelectList'){
+        data='searchData';
+    }
+    if(miss>find&&find==0&&miss!=0){
+        document.getElementById(data+'DOD').style.display="block";
+    }else{
+        document.getElementById(data+'DOD').style.display="none";
+    }
+}
+System.prototype.downloadImage = function(id, name){
+    const img = document.getElementById(id);
+    const fileName = name || '1.png';
+    const link = document.createElement("a");
+    link.href = img.src;
+    link.download = fileName;
+    link.click();
+}
